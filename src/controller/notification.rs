05f5bf557b774fcad43b5db2828eba3e6c294f17
unknown <playarisa24@gmail.com>
2026-03-13 00:30:04 +0700

    fix typo on notifcation controller

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -1,6 +1,6 @@
 use rocket::response::status::Created;
 use rocket::serde::json::Json;
 
-use bambangship::Result;
+use bambangshop::Result;
 use crate::model::subscriber::Subscriber;
 use crate::service::notification::NotificationService;
