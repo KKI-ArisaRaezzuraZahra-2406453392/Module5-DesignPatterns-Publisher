6d99c7eaee43a929dad32de6679966ea29e704e8
unknown <playarisa24@gmail.com>
2026-03-30 23:43:06 +0700

    fix typos

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -5,9 +5,9 @@ use bambangshop::Result;
 use crate::model::subscriber::Subscriber;
 use crate::service::notification::NotificationService;
 
-#[post("/subscribe/<product_type", data ="<subscriber>")]
+#[post("/subscribe/<product_type>", data ="<subscriber>")]
 pub fn subscribe(product_type: &str, subscriber: Json<Subscriber>) -> Result<Created<Json<Subscriber>>> {
-    return match NotificationService::subscrive(product_type, subscriber.into_inner()) {
+    return match NotificationService::subscribe(product_type, subscriber.into_inner()) {
         Ok(f) => Ok(Created::new("/").body(Json::from(f))),
         Err(e) => Err(e)
     };
